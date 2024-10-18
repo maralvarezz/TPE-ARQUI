ae4ffb3d8ae4d92da81bce80d63288c2794a77c9
SantiSanchezMar <ssanchezmarostica@itba.edu.ar>
2024-10-18 14:21:29 -0300

    fix

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,6 +1,6 @@
 /* sampleCodeModule.c */
 #include "libraryC.h"
-char * v = (char*)0xB8000 + 79 * 2;
+char * v = (char*)0xB8000 + 80 * 2;
 
 static int var1 = 0;
 static int var2 = 0;
@@ -11,10 +11,10 @@ int main() {
 	printTime(v);
 	//corrige donde se ve en consola
 	v+=12;
-	while(1){
+	/*while(1){
 		keyboard(v);
 		v++;
-	}
+	}*/
 	if (var1 == 0 && var2 == 0)
 		return 0xDEADC0DE;
 
