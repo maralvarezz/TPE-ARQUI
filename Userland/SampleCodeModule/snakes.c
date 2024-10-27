3f80feb4b435527be6471420f9ef6e383561cc0b
SantiSanchezMar <ssanchezmarostica@itba.edu.ar>
2024-10-27 20:45:31 -0300

    feat: snakes
    Co-Authored-By: Martin Gallardo Bertuzzi <146043951+Martin192003@users.noreply.github.com>
    Co-Authored-By: Maria Victoria Alvarez <146204185+maralvarezz@users.noreply.github.com>

--- Userland/SampleCodeModule/snakes.c
+++ Userland/SampleCodeModule/snakes.c
@@ -11,6 +11,17 @@ void startGame(char jug){
 }
 
 /*
+//variable global para los jugadores
+char cantjug;
+
+int passengerX, passengerY;
+
+int direc[4] = {
+    {0, 1}, //derecha
+    {0, -1}, //izquierda
+    {1, 0}, //abajo
+    {-1, 0} //arriba
+}
 
 typedef struct player{
     int posX;
@@ -18,6 +29,7 @@ typedef struct player{
     int dirX;
     int dirY;
     int points;
+    //faltaria el color
 }player;
 
 typedef struct player * TPlayer;
@@ -40,14 +52,53 @@ typedef struct player * TPlayer;
     }
 
     //devulve 0 si no hay colision
-    int checkCollision(TPlayer p){)
+    int collision(TPlayer p){
+        if(p->posX + p->dirX >= BOARD_SIZE || p->posY + p->dirY >= BOARD_SIZE){
+            return 1;
+        }
+        else if(cantjug == 2 && // fijarse colision con otro jugador){
+            //una idea seria que cada jugador tuviera su matriz donde se muestra dependiendo de sus puntos y como se fue moviendo cuanto y donde ocupa y se comparan matrices para ver si hay colision
+            //idea fea y aspera
+            return 1;
+        }
+        return 0;
+    }
+
+    int checkPassenger(TPlayer p){
+        if(p->posX == passengerX && p->posY == passengerY){
+            return 1;
+        }
     }
 
     void movePlayer(TPlayer p){
-        if(checkCollision(p)){
+        if(collision(p)){
             l = 0;
-        }    
+        }
+        else{
+            if(checkPassenger(p)){
+                p->points++;
+            }
+            p->posX += p->dirX;
+            p->posY += p->dirY;
+        }
     }
+
+    void setDirection(TPlayer p, int dir){
+        p->dirX = direc[dir][0];
+        p->dirY = direc[dir][1];
+
+    }
+
+    static int random(){
+        return gettingSeconds();
+    }
+
+    void setPassenger(){
+        passengerX = random() % BOARD_SIZE;
+        passengerY = random() % BOARD_SIZE;
+    }
+
+
 */
 
 
