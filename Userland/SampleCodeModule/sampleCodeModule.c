07dd718f16a9e83e765a774a9d138de61b0376f3
SantiSanchezMar <ssanchezmarostica@itba.edu.ar>
2024-10-28 18:11:26 -0300

    ANDAN LOS COLORES
    Co-Authored-By: Martin Gallardo Bertuzzi <146043951+Martin192003@users.noreply.github.com>
    Co-Authored-By: Maria Victoria Alvarez <146204185+maralvarezz@users.noreply.github.com>

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,9 +1,9 @@
 /* sampleCodeModule.c */
-/*#include "./include/shell.h"
+#include "./include/shell.h"
 #define SQUARESIZE 50
 
-ColorT color1={0xB6, 0xE8, 0xE4};
-ColorT color2={0xCD,0xF4,0xF1};
+ColorT color1={0xBB, 0xAA, 0x0A};
+ColorT color2={0xAF,0xFA,0xAA};
 int main() {
     //entry();
     //terminal();
@@ -20,48 +20,5 @@ int main() {
             //drawRect(x, y, SQUARESIZE, SQUARESIZE, &color);
         }
     }
-
-
-    return 0;
-}
-*/
-
-#include "./include/shell.h"
-#define SQUARESIZE 50
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
-//HAY QUILOMBO DE COLORES
-ColorT color1 = {1,0,0};
-ColorT color2 = {0,0,0};
-
-void drawGrid() {
-    int rows = SCREEN_HEIGHT / SQUARESIZE;
-    int cols = SCREEN_WIDTH / SQUARESIZE;
-    int row=0;
-    /*for (int row = 0; row < rows; row++) {
-        for (int col = 0; col < cols; col++) {
-            int x = col * SQUARESIZE;
-            int y = row * SQUARESIZE;
-            if(col%2){
-                drawRect(0, y, SQUARESIZE, SQUARESIZE, &color1);    
-            }else{
-                drawRect(0, y, SQUARESIZE, SQUARESIZE, &color2);
-            }
-            //ColorT color = row % 2 == 0 ? color1 : color2;
-            
-        }
-    }*/
-
-
-
-    drawRect(0, 0, SQUARESIZE, SQUARESIZE, &color2);
-    drawRect(0, 50, SQUARESIZE, SQUARESIZE, &color1);
-    drawRect(0, 100, SQUARESIZE, SQUARESIZE, &color2);
-    drawRect(0, 150, SQUARESIZE, SQUARESIZE, &color1);
-    drawRect(0, 200, SQUARESIZE, SQUARESIZE, &color2);
-}
-
-int main() {
-    drawGrid();
     return 0;
 }
