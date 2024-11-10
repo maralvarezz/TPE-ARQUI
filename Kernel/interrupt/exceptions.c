fe913d6d7bb0c19a4c1cd121e811168bdd3ca3c6
MartinGallardoB <mgallardobertuzzi@itba.edu.ar>
2024-11-10 12:59:32 -0300

    solucionado todo

--- Kernel/interrupt/exceptions.c
+++ Kernel/interrupt/exceptions.c
@@ -11,6 +11,7 @@ extern const uint64_t exceptionRegs[REGISTERS];
 static char * regs[REGISTERS] = {
 "RAX","RBX","RCX","RDX","RSI","RDI","RBP","R8","R9","R10","R11","R12","R13","R14","R15","RIP","CS", "RFLAGS", "RSP"
 };
+static void pasoHexa(uint64_t num1, char buffer[16]);
 
 void exceptionDispatcher(int exception) {
 	if (exception == ZERO_EXCEPTION_ID)
@@ -20,12 +21,24 @@ void exceptionDispatcher(int exception) {
 }
 
 void print_registers() {
-	for (int i = 0; i < 19; i++) {
-		print(regs[i], strleng(regs[i]));
-		print(": ", 2);
-		print(numToStr(exceptionRegs[i], 18),18);
-		print("\n",1);
-	}
+    char buffer[16];
+    for (int i = 0; i < 19; i++) {
+        print(regs[i], strleng(regs[i]));
+        print(": ", 2);
+        pasoHexa(exceptionRegs[i], buffer);
+        print(buffer, 16);
+        print("\n", 1);
+    }
+}
+
+static void pasoHexa(uint64_t num1, char buffer[16]){
+	int i = 15;
+	do
+	{
+		int num2 = num1 % 16;
+		buffer[i] = (num2 < 10 ? '0' : ('A' - 10)) + num2;
+		num1 /= 16;
+	} while (i-- != 0);
 }
 
 static void zero_division() {
